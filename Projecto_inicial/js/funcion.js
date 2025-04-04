692c65f696bcb666f55ac2fb0e4ffc019bf6f9a9
JonY-Vinan <jony.v.c.ecuador95@gmail.com>
2025-04-04 10:47:04 +0200

    formulario

--- Projecto_inicial/js/funcion.js
+++ Projecto_inicial/js/funcion.js
@@ -7,13 +7,13 @@ function cambiarTable() {
   var tabla = document.getElementById("tabla1");
   var tabla2 = document.getElementById("tabla2");
   var btnText = document.getElementById("btn");
-  if (tabla.style.display === "inline") {
+  if (tabla.style.display === "inline-table") {
     tabla.style.display = "none";
-    tabla2.style.display = "inline";
+    tabla2.style.display = "inline-table";
     btnText.innerHTML = "Tabla 1";
   } else {
     tabla2.style.display = "none";
-    tabla.style.display = "inline";
+    tabla.style.display = "inline-table";
     btnText.innerHTML = "Tabla 2";
   }
 }
