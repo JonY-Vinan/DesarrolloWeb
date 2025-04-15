388a6d056f91af394d4b01befb9fbf75cf2dd9bc
JonY-Vinan <jony.v.c.ecuador95@gmail.com>
2025-04-15 14:52:10 +0200

    añadido botones resumen y borrar y son funcionales

---
+++ Alumno/Alumno.js
@@ -0,0 +1,42 @@
+export class Alumno {
+  constructor(dni, nombre, apellido, edad, curso) {
+    this.dni = this.validarDni(dni);
+    this.nombre = this.validarTexto(nombre);
+    this.apellido = this.validarTexto(apellido);
+    this.edad = this.validarEdad(edad);
+    this.curso = curso;
+  }
+
+  static cursos() {
+    return ["Informatica", "Medicina", "Fontanero", "Pitor", "Cocinero"];
+  }
+
+  validarDni(dni) {
+    const regex = /^[0-9]{8}[A-Z]$/;
+    if (!regex.test(dni)) {
+      window.alert("DNI INCORRECTO");
+      throw new Error("DNI INCORRECTO");
+    } else {
+      return dni;
+    }
+  }
+  validarEdad(edad) {
+    if (edad >= 18 && edad <= 120) {
+      return edad;
+    } else {
+      throw new Error("Error en la edad");
+    }
+  }
+
+  validarTexto(texto) {
+    if (typeof texto !== "string") {
+      throw new Error("Los campos nombre y apellido no debe contener numeros");
+    } else {
+      return texto;
+    }
+  }
+
+  static resumen(dni, nombre, apellido, edad, curso) {
+    return `El DNI: ${dni} del alumno ${nombre} ${apellido} de ${edad} años que ahora esta inscrito en el curso de ${curso}`;
+  }
+}
---
+++ Alumno/appAlumno.js
@@ -0,0 +1,151 @@
+import { Alumno } from "./Alumno.js";
+let listadoAlumno = [];
+let claveAlumno = {};
+//PARA CREAR UN NUEVO USUARIO
+document
+  .getElementById("nuevo_alumno")
+  .addEventListener("click", () => crearNuevoAlumno());
+
+function crearNuevoAlumno() {
+  let alumno = obtenerDatos();
+  console.log(alumno);
+  listadoAlumno.push(alumno);
+  console.log(listadoAlumno);
+  localStorage.setItem("alumnos", JSON.stringify(listadoAlumno));
+  inicioTabla(listadoAlumno);
+  anadirFila();
+}
+
+function obtenerDatos() {
+  let dni = document.getElementById("dni").value;
+  let nombre = document.getElementById("nombre").value;
+  let apellido = document.getElementById("apellido").value;
+  let edad = document.getElementById("edad").value;
+  let curso = document.getElementById("cursos").value;
+  let nuevoAlumno = new Alumno(dni, nombre, apellido, edad, curso);
+  return nuevoAlumno;
+}
+
+function cargarCursos() {
+  let selector = document.getElementById("cursos");
+  let curso = Alumno.cursos();
+  selector.innerHTML = "";
+  for (let i = 0; i < curso.length; i++) {
+    let opciones = document.createElement("option");
+    opciones.innerText = curso[i];
+    selector.appendChild(opciones);
+  }
+}
+cargarCursos();
+
+function anadirFila() {
+  let tablaAlumnos = document.getElementById("tabla_alumnos");
+  tablaAlumnos.innerHTML = "";
+  for (let indice = 0; indice < listadoAlumno.length; indice++) {
+    let fila = document.createElement("tr");
+    let btn1 = document.createElement("td");
+    let btn2 = document.createElement("td");
+    console.log(listadoAlumno);
+
+    let almn = listadoAlumno[indice];
+    fila.innerHTML = "";
+    btn1.innerHTML = "";
+    btn2.innerHTML = "";
+    fila.innerHTML = `<th scope="row">${listadoAlumno[indice].dni}</th>
+      <td>${listadoAlumno[indice].nombre}</td>
+      <td>${listadoAlumno[indice].apellido}</td>
+      <td>${listadoAlumno[indice].edad}</td>
+      <td>${listadoAlumno[indice].curso}</td>`;
+    btn1.appendChild(botonResumen(almn));
+    fila.appendChild(btn1);
+    btn2.appendChild(botonBorrar(almn), indice);
+    fila.appendChild(btn2);
+    tablaAlumnos.appendChild(fila);
+    listadoAlumno.push(listadoAlumno[indice]);
+  }
+  // listadoAlumno.push((listadoAlumno[3].email = "ggg"));
+  // let otro = obtenerDatos(dni: '12345678P', nombre: 'a', apellido: 'a', edad: '23', curso: 'Informatica'. email: 'ecua@gmail.com')
+}
+
+function inicioTabla(listadoAlumno) {
+  let tabla = document.getElementById("tabla");
+  let inicio = document.getElementById("tabla_inicio");
+  inicio.innerHTML = "";
+  claveAlumno = listadoAlumno[0];
+  let fila = document.createElement("tr");
+  fila.id = "id_tr";
+  fila.innerHTML = "";
+  for (let clave in claveAlumno) {
+    let fila1 = document.createElement("th");
+    fila1.innerHTML = "";
+    fila1.innerText = clave;
+    fila.appendChild(fila1);
+    // console.log(fila);
+  }
+
+  let resumen = document.createElement("th");
+  resumen.innerText = `Ver resumen`;
+  let borrar = document.createElement("th");
+  borrar.innerText = `Borrar`;
+  fila.appendChild(resumen);
+  fila.appendChild(borrar);
+  inicio.appendChild(fila);
+  tabla.appendChild(inicio);
+}
+
+document
+  .getElementById("cargar_alumnos")
+  .addEventListener("click", () => cargarAlumnos());
+function cargarAlumnos() {
+  const datos = JSON.parse(localStorage.getItem("alumnos")) || [];
+  listadoAlumno = datos.map(
+    (a) => new Alumno(a.dni, a.nombre, a.apellido, a.edad, a.curso)
+  );
+
+  inicioTabla(listadoAlumno);
+  anadirFila();
+}
+
+function botonResumen(listadoAlumno) {
+  let btnResumen = document.createElement("button");
+  btnResumen.innerHTML = "";
+  btnResumen.textContent = "Resumen";
+  btnResumen.addEventListener("click", function () {
+    const div = document.getElementById("alumnoResumen");
+    div.innerHTML = "";
+    let resumen = Alumno.resumen(
+      listadoAlumno.dni,
+      listadoAlumno.nombre,
+      listadoAlumno.apellido,
+      listadoAlumno.edad,
+      listadoAlumno.curso
+    );
+    console.log(resumen);
+    div.append(resumen);
+  });
+  return btnResumen;
+}
+
+function botonBorrar(alumno, index) {
+  let btnResumen = document.createElement("button");
+  btnResumen.innerHTML = "";
+  btnResumen.textContent = "Borrar";
+  btnResumen.addEventListener("click", function () {
+    if (
+      confirm(
+        `¿Estás seguro de querer borrar a ${alumno.nombre} ${alumno.apellido}?`
+      )
+    ) {
+      // Eliminar el alumno del array
+      listadoAlumno.splice(index, 1);
+
+      // Actualizar la tabla
+      anadirFila();
+
+      // Opcional: Mostrar mensaje de éxito
+      console.log("Alumno borrado correctamente");
+    }
+  });
+
+  return btnResumen;
+}
